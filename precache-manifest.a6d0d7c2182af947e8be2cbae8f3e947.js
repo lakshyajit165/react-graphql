65e909ddd5374e07a51b995f80e32072e00ef2a1
Lakshyajit <lakshyajit@localhost.localdomain>
2019-05-12 11:50:57 +0530

    Updates

---
+++ precache-manifest.a6d0d7c2182af947e8be2cbae8f3e947.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "742057f8de61ed43edb229ae00b73d47",
+    "url": "/react-graphql/index.html"
+  },
+  {
+    "revision": "d0671691d9427fb099b5",
+    "url": "/react-graphql/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "711538b2be2afbe62d20",
+    "url": "/react-graphql/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "d0671691d9427fb099b5",
+    "url": "/react-graphql/static/js/2.8c8588ee.chunk.js"
+  },
+  {
+    "revision": "711538b2be2afbe62d20",
+    "url": "/react-graphql/static/js/main.c0bca537.chunk.js"
+  },
+  {
+    "revision": "710c91f59a7c0ed26e06",
+    "url": "/react-graphql/static/js/runtime~main.6cdb7eb0.js"
+  }
+]);
